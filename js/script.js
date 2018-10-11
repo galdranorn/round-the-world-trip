19a77a495c7837a5446fee810feaa4ebc9c57dea
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-10-11 19:40:52 +0200

    Add progress bar

--- js/script.js
+++ js/script.js
@@ -23,3 +23,12 @@ var refreshBtn = document.querySelector("#refresh");
 refreshBtn.addEventListener('click', function() {
     flkty.select(0)
 });
+
+// progress bar 
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
