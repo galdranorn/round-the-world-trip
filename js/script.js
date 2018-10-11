f9b5be10c70bf16b02bc16c0c91bca6afa54b24d
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-10-11 21:58:23 +0200

    Add google map

--- js/script.js
+++ js/script.js
@@ -47,3 +47,32 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+// ----------------------------------------
+// google map
+// ----------------------------------------
+
+(function(){ 
+	
+	// Definujemy funkcję initMap w zakresie globalnym (czyli jako właściwość obiektu window).
+  	window.initMap = function() {
+		
+		// Zapisujemy w zmiennej obiekt zawierający współrzędne geograficzne.
+		var uluru = {lat: -25.363, lng: 131.044};
+		
+		// W zmiennej map zapisujemy nową instancję obiektu Map. 
+		var map = new google.maps.Map(document.getElementById('map'), {
+			// Podajemy opcje mapy, np. zoom i punkt wycentrowania mapy.
+			zoom: 4,
+			center: uluru
+		});
+		
+		// Definiujemy marker jako nową instancję obiektu Marker.
+		var marker = new google.maps.Marker({
+			// I podajemy opcje tego markera, np. na której mapie ma być dodany oraz jakie są jego współrzędne. 
+			position: uluru,
+			map: map
+		}); 
+	}	
+	 
+})();  
